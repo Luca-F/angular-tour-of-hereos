0d516651da8a440f066003bccc62eac56ffa45c1
Luca-F <72333806+Luca-F@users.noreply.github.com>
2023-01-24 18:14:12 +0100

    2 files modified

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -18,7 +18,7 @@ export class HeroDetailComponent {
     private heroService: HeroService,
     private location: Location
   ) {}
-  
+
   ngOnInit(): void {
     this.getHero();
   }
@@ -27,5 +27,9 @@ export class HeroDetailComponent {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
