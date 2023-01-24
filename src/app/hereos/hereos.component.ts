c16f663f4623d738d93ab620663a92c1a781d7ee
Luca-F <72333806+Luca-F@users.noreply.github.com>
2023-01-24 16:57:09 +0100

    2 files modified, 1 added

--- src/app/hereos/hereos.component.ts
+++ src/app/hereos/hereos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-hereos',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./hereos.component.css']
 })
 export class HereosComponent {
-  hero = 'Windstorm';
+  hero: Hero ={
+    id: 1,
+    name: 'Windstorm'
+  };
 }
